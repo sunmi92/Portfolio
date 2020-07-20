9834d7ef24e1460678a86f4eeb5bf887c301e800
sunmi92 <65153255+sunmi92@users.noreply.github.com>
2020-07-20 19:04:40 +0900

    Add files via upload

---
+++ resource/js/matterport_custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#cta-container > cta > h1').html('클릭하여 3D 공간 탐색해 보세요.');
+    $('#cta-container > cta > h2:nth-of-type(1)').html('둘러보기');
+    $('#cta-container > cta > h2:nth-of-type(2)').html('이동');
+    $('#cta-container > cta > h2:nth-of-type(3)').html('플레이');
+})
